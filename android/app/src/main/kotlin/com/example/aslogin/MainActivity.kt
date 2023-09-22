d75a5568be8d16b5ee74767987e90b711b187aeb
Ayesha Akram shahab <108744690+ayeshashahab@users.noreply.github.com>
2023-09-22 18:25:28 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aslogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aslogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:day12_login/Animation/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+      	child: Container(
+	        child: Column(
+	          children: <Widget>[
+	            Container(
+	              height: 400,
+	              decoration: BoxDecoration(
+	                image: DecorationImage(
+	                  image: AssetImage('assets/images/background.png'),
+	                  fit: BoxFit.fill
+	                )
+	              ),
+	              child: Stack(
+	                children: <Widget>[
+	                  Positioned(
+	                    left: 30,
+	                    width: 80,
+	                    height: 200,
+	                    child: FadeAnimation(1, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/light-1.png')
+	                        )
+	                      ),
+	                    )),
+	                  ),
+	                  Positioned(
+	                    left: 140,
+	                    width: 80,
+	                    height: 150,
+	                    child: FadeAnimation(1.3, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/light-2.png')
+	                        )
+	                      ),
+	                    )),
+	                  ),
+	                  Positioned(
+	                    right: 40,
+	                    top: 40,
+	                    width: 80,
+	                    height: 150,
+	                    child: FadeAnimation(1.5, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/clock.png')
+	                        )
+	                      ),
+	                    )),
+	                  ),
+	                  Positioned(
+	                    child: FadeAnimation(1.6, Container(
+	                      margin: EdgeInsets.only(top: 50),
+	                      child: Center(
+	                        child: Text("Welcome to IUB", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+	                      ),
+	                    )),
+	                  )
+	                ],
+	              ),
+	            ),
+	            Padding(
+	              padding: EdgeInsets.all(30.0),
+	              child: Column(
+	                children: <Widget>[
+	                  FadeAnimation(1.8, Container(
+	                    padding: EdgeInsets.all(5),
+	                    decoration: BoxDecoration(
+	                      color: Colors.white,
+	                      borderRadius: BorderRadius.circular(10),
+	                      boxShadow: [
+	                        BoxShadow(
+	                          color: Color.fromARGB(51, 102, 109, 238),
+	                          blurRadius: 20.0,
+	                          offset: Offset(0, 10)
+	                        )
+	                      ]
+	                    ),
+	                    child: Column(
+	                      children: <Widget>[
+	                        Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          decoration: BoxDecoration(
+	                            border: Border(bottom: BorderSide(color: Colors.grey[100]))
+	                          ),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Email or Phone number",
+	                              hintStyle: TextStyle(color: Color.fromARGB(255, 201, 197, 197))
+	                            ),
+	                          ),
+	                        ),
+	                        Container(
+	                          padding: EdgeInsets.all(8.0),
+	                          child: TextField(
+	                            decoration: InputDecoration(
+	                              border: InputBorder.none,
+	                              hintText: "Password",
+	                              hintStyle: TextStyle(color: Colors.grey[400])
+	                            ),
+	                          ),
+	                        )
+	                      ],
+	                    ),
+	                  )),
+	                  SizedBox(height: 30,),
+	                  FadeAnimation(2, Container(
+	                    height: 50,
+	                    decoration: BoxDecoration(
+	                      borderRadius: BorderRadius.circular(10),
+	                      gradient: LinearGradient(
+	                        colors: [
+	                          Color.fromARGB(255, 42, 47, 156),
+	                          Color.fromARGB(153, 69, 77, 228),
+	                        ]
+	                      )
+	                    ),
+	                    child: Center(
+	                      child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+	                    ),
+	                  )),
+	                  SizedBox(height: 70,),
+	                  FadeAnimation(1.5, Text("Forgot Password?", style: TextStyle(color: Color.fromARGB(255, 57, 59, 89)),)),
+	                ],
+	              ),
+	            )
+	          ],
+	        ),
+	      ),
+      )
+    );
+  }
+}
